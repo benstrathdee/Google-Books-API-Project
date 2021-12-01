51695aaf0b2c5a0f5f010f356ec0eb84557de857
Ben Strathdee <ben.strathdee@live.com.au>
2021-12-01 16:51:26 +1100

    added functionality for resultsnum

--- src/containers/Navbar/Navbar.jsx
+++ src/containers/Navbar/Navbar.jsx
@@ -1,16 +1,16 @@
-import styles from "./Navbar.module.scss"
-import Search from "./../../components/Search"
-import PageSelector from "../../components/PageSelector/PageSelector"
-import ResultsNum from "../../components/ResultsNum/ResultsNum"
+import styles from "./Navbar.module.scss";
+import Search from "./../../components/Search";
+import PageSelector from "../../components/PageSelector/PageSelector";
+import ResultsNum from "../../components/ResultsNum/ResultsNum";
 
-const Navbar = ({click, update}) => {
-    return (
-        <div className={styles.Navbar}>
-            <Search click={click} update={update}/>
-            <PageSelector />
-            <ResultsNum />
-        </div>
-    )
-}
+const Navbar = ({ click, search, maxResults }) => {
+	return (
+		<div className={styles.Navbar}>
+			<Search click={click} search={search} />
+			<PageSelector />
+			<ResultsNum maxResults={maxResults} />
+		</div>
+	);
+};
 
-export default Navbar
+export default Navbar;
