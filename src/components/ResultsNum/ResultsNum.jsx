0c329b36f49cf90404f3c129a8035804a340f639
Ben Strathdee <ben.strathdee@live.com.au>
2021-12-01 16:50:13 +1100

    added functionality to select

--- src/components/ResultsNum/ResultsNum.jsx
+++ src/components/ResultsNum/ResultsNum.jsx
@@ -1,18 +1,17 @@
-import styles from "./ResultsNum.module.scss"
+import styles from "./ResultsNum.module.scss";
 
-const ResultsNum = () => {
-    return (
-        <div className={styles.ResultsNum}>
-            <p>Results per page: </p>
-            <select className={styles.ResultsNum_select}>
-                <option value="10">10</option>
-                <option value="20">20</option>
-                <option value="30">30</option>
-                <option value="40">40</option>
-                <option value="50">50</option>
-            </select>
-        </div>
-    )
-}
+const ResultsNum = ({ maxResults }) => {
+	return (
+		<div className={styles.ResultsNum}>
+			<p>Results per page: </p>
+			<select className={styles.ResultsNum_select} onChange={maxResults}>
+				<option value="10">10</option>
+				<option value="20">20</option>
+				<option value="30">30</option>
+				<option value="40">40</option>
+			</select>
+		</div>
+	);
+};
 
-export default ResultsNum
+export default ResultsNum;
