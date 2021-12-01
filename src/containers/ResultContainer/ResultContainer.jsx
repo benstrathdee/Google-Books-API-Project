92137d9dc0d3485e4b199ca258198600568e8503
Ben Strathdee <ben.strathdee@live.com.au>
2021-12-01 16:52:18 +1100

    added handling for no images, pass down more info to result box

--- src/containers/ResultContainer/ResultContainer.jsx
+++ src/containers/ResultContainer/ResultContainer.jsx
@@ -1,19 +1,36 @@
-import styles from "./ResultContainer.module.scss"
+import styles from "./ResultContainer.module.scss";
 import Result from "../../components/Result/Result";
 
-const ResultContainer = ({data}) => {
-    return (
-        <div className={styles.ResultContainer}>
-            {data.map((book, index) => { return (
-                <Result
-                    key = {index}
-                    content = {book.volumeInfo.title}
-                    author = {book.volumeInfo.authors}
-                    thumbnail = {book.volumeInfo.imageLinks.thumbnail}
-                />
-            )})}
-        </div>
-    )
-}
+const ResultContainer = ({ data, modalOp, modalContent }) => {
+	return data === undefined ? (
+		<p className={styles.ResultContainer_blank}>No results found.</p>
+	) : (
+		<div className={styles.ResultContainer}>
+			{data.map((book, index) => {
+				const image =
+					book.volumeInfo.imageLinks === undefined
+						? "https://www.ncenet.com/wp-content/uploads/2020/04/No-image-found.jpg"
+						: book.volumeInfo.imageLinks.thumbnail;
 
-export default ResultContainer
+				return (
+					<>
+						<Result
+							key={index}
+							title={book.volumeInfo.title}
+							authors={book.volumeInfo.authors}
+							thumbnail={image}
+							desc={book.volumeInfo.subtitle}
+							release={book.volumeInfo.publishedDate}
+							pages={book.volumeInfo.pageCount}
+							link={book.volumeInfo.previewLink}
+							modalOp={modalOp}
+							modalContent={modalContent}
+						/>
+					</>
+				);
+			})}
+		</div>
+	);
+};
+
+export default ResultContainer;
