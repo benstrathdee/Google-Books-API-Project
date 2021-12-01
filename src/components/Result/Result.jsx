8bfd2a1b175fc4e890cca25d7b47b2f094fd47b4
Ben Strathdee <ben.strathdee@live.com.au>
2021-12-01 16:49:43 +1100

    added modal handling, updated appearence

--- src/components/Result/Result.jsx
+++ src/components/Result/Result.jsx
@@ -1,13 +1,42 @@
-import styles from "./Result.module.scss"
+import styles from "./Result.module.scss";
 
-const Result = ({content, author, thumbnail}) => {
-    return (
-        <div className={styles.ResultCard}>
-            <p>{content}</p>
-            <p>{author}</p>
-            <img src={thumbnail} alt="" />
-        </div>
-    )
-}
+const Result = ({
+	title,
+	authors,
+	thumbnail,
+	desc,
+	release,
+	pages,
+	link,
+	modalOp,
+	modalContent,
+}) => {
+	const sendToModal = () => {
+		modalContent([title, authors, desc, release, pages, link]);
+		modalOp();
+	};
 
-export default Result
+	const truncateString = (str, num, end) => {
+		if (str.length <= num) {
+			return str;
+		}
+		return str.slice(0, num) + end;
+	};
+
+	const authorList =
+		authors === undefined
+			? "Author Unknown"
+			: authors.length < 3
+			? authors.join(", ")
+			: `${authors[0]} et al.`;
+
+	return (
+		<div className={styles.ResultCard} onClick={sendToModal}>
+			<h2>{truncateString(title, 50, "...")}</h2>
+			<h6>{truncateString(authorList, 40, "...")}</h6>
+			<img src={thumbnail} alt="" className={styles.ResultCard_thumb} />
+		</div>
+	);
+};
+
+export default Result;
