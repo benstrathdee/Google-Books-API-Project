80f843d52fe67a2a7afe05ef1f078b06c665f2c0
Ben Strathdee <ben.strathdee@live.com.au>
2021-12-01 16:50:48 +1100

    fixed functionality

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,15 +1,21 @@
-import styles from "./Search.module.scss"
-import { faSearch } from "@fortawesome/free-solid-svg-icons"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import styles from "./Search.module.scss";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-const Search = ({click, update}) => {
-    const searchIcon = <FontAwesomeIcon icon={faSearch} />
-    return (
-        <div>
-            <input type="text" onChange={update} className={styles.Search_input}/>
-            <button onClick={click} className={styles.Search_btn}>{searchIcon}</button>
-        </div>
-    )
-}
+const Search = ({ click, search }) => {
+	const searchIcon = <FontAwesomeIcon icon={faSearch} />;
+	return (
+		<div>
+			<input
+				type="text"
+				onChange={search}
+				className={styles.Search_input}
+			/>
+			<button onClick={click} className={styles.Search_btn}>
+				{searchIcon}
+			</button>
+		</div>
+	);
+};
 
-export default Search
+export default Search;
