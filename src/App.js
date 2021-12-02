c6a2c9c3f8cecc05468d4139eca553de12b8e098
Ben Strathdee <ben.strathdee@live.com.au>
2021-12-02 22:29:22 +1100

    added more hooks, functions to support components, footer, tidied code

--- src/App.js
+++ src/App.js
@@ -2,33 +2,40 @@ import styles from "./App.module.scss";
 import Navbar from "./containers/Navbar";
 import ResultContainer from "./containers/ResultContainer";
 import Modal from "./components/Modal/Modal";
+import Footer from "./containers/Footer/Footer";
 import { useState, useEffect } from "react";
 
 function App() {
-	const GOOGLE_BOOKS_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+	const GOOGLE_BOOKS_URL = "https://www.googleapis.com/books/v1/volumes";
+	const defaultSearchTerm = "HTML";
 
 	const [booksList, setBooksList] = useState([]);
-	const [searchTerm, setSearchTerm] = useState("a");
+	const [searchTerm, setSearchTerm] = useState(defaultSearchTerm);
 	const [doSearch, setDoSearch] = useState(false);
-	const [maxResults, setMaxResults] = useState(10);
+
 	const [showModal, setShowModal] = useState(false);
 	const [modalContent, setModalContent] = useState([]);
 
-	const getBooks = async (searchTerm = "a", maxResults = 10) => {
-		const response = await fetch(
-			`${GOOGLE_BOOKS_URL}${searchTerm}&maxResults=${maxResults}`
-		);
-		const data = await response.json();
-		const items = await data.items;
-		setBooksList(items);
-	};
+	const [totalResults, setTotalResults] = useState([0]);
+	const [maxResults, setMaxResults] = useState(10);
+
+	const [startIndex, setStartIndex] = useState(0);
+	const [currentPage, setCurrentPage] = useState(1);
+	const [pagesToList, setPagesToList] = useState([]);
+	const [totalPages, setTotalPages] = useState(
+		Math.ceil(totalResults / maxResults) > 50
+			? 50
+			: Math.ceil(totalResults / maxResults)
+	);
 
 	const handleDoSearch = () => {
 		setDoSearch(!doSearch);
 	};
 
 	const handleSearchTerm = (event) => {
-		setSearchTerm(event.target.value === "" ? "a" : event.target.value);
+		setSearchTerm(
+			event.target.value === "" ? defaultSearchTerm : event.target.value
+		);
 	};
 
 	const handleResultNum = (event) => {
@@ -44,8 +51,73 @@ function App() {
 	};
 
 	useEffect(() => {
+		setTotalPages(
+			Math.ceil(totalResults / maxResults) > 50
+				? 50
+				: Math.ceil(totalResults / maxResults)
+		);
+	}, [maxResults, totalResults]);
+
+	useEffect(() => {
+		setPagesToList([
+			parseInt(currentPage) - 2,
+			parseInt(currentPage) - 1,
+			parseInt(currentPage),
+			parseInt(currentPage) + 1,
+			parseInt(currentPage) + 2,
+		]);
+	}, [currentPage]);
+
+	const handlePages = (event) => {
+		window.scrollTo(0, 0);
+		switch (event.target.value) {
+			case "start":
+				setStartIndex(0);
+				setCurrentPage(1);
+				break;
+			case "prev":
+				setStartIndex((parseInt(currentPage) - 2) * maxResults);
+				setCurrentPage(parseInt(currentPage) - 1);
+				break;
+			case "next":
+				setStartIndex(parseInt(currentPage) * maxResults);
+				setCurrentPage(parseInt(currentPage) + 1);
+				break;
+			case "end":
+				setStartIndex((totalPages - 1) * maxResults);
+				setCurrentPage(totalPages);
+				break;
+			default:
+				setStartIndex((event.target.value - 1) * maxResults);
+				setCurrentPage(event.target.value);
+		}
+	};
+
+	const getBooks = async (
+		searchTerm = defaultSearchTerm,
+		maxResults = 10,
+		startIndex = 0
+	) => {
+		window.scrollTo(0, 0);
+		const response = await fetch(
+			`${GOOGLE_BOOKS_URL}
+				?q=${searchTerm}
+				&maxResults=${maxResults}
+				&startIndex=${startIndex}`
+		);
+		const data = await response.json();
+		const items = await data.items;
+		setBooksList(items);
+		setTotalResults(data.totalItems);
+	};
+
+	useEffect(() => {
+		getBooks(searchTerm, maxResults, startIndex);
+	}, [maxResults, startIndex]);
+
+	useEffect(() => {
 		getBooks(searchTerm, maxResults);
-	}, [doSearch, maxResults]);
+	}, [doSearch]);
 
 	return (
 		<div className={styles.App}>
@@ -53,6 +125,7 @@ function App() {
 				click={handleDoSearch}
 				search={handleSearchTerm}
 				maxResults={handleResultNum}
+				totalResults={totalResults}
 			/>
 			<ResultContainer
 				data={booksList}
@@ -64,6 +137,7 @@ function App() {
 				showModal={showModal}
 				content={modalContent}
 			/>
+			<Footer pagesToList={pagesToList} handlePages={handlePages} />
 		</div>
 	);
 }
